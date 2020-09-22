6ff1dbf82584757d01c426e049e46a25337d88a0
Bianca SchoenPhelan <bianca.schoenphelan@soc-mbp13-bsp.lan>
2020-09-22 14:43:48 +0100

    File Lab 1

---
+++ Labs/lab1.py
@@ -0,0 +1,17 @@
+# Object oriented programming
+# semester 1: Python
+# Dr Bianca Schoen-Phelan
+# Sep 2020
+# Code for Lab 1: first steps
+# task: run this code, play with it,
+# do you understand what happens?
+
+class FirstLab:
+    def __init__(self):
+        print("hi, this is my first python class")
+
+
+fl = FirstLab()
+
+# uncomment the following line, play with it
+# print("I can print anywhere I like")
